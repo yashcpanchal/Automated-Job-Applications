37b8c44dfa68b2eea390ecd5d041875b854d980e
yashcpanchal <yashcpanchal41@gmail.com>
2025-05-31 00:02:09 -0400

    Added the embedding fucntionality using all-MiniLM-L6-v2 from sentence transformers

--- application-backend/main.py
+++ application-backend/main.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI, Depends
-from typing import Optional, Annotated # We'll use this later for optional secrets
+from typing import Optional, Annotated # These are used for type hinting
+from datetime import datetime # Used for timestamp 
 import pymongo
 import os
 from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer
 
 app = FastAPI()
 
+# loads the .env file
+load_dotenv()
+
+# Mongo vars
 MONGODB_URI = os.getenv("MONGODB_URI")
-DATABASE_NAME = "job_applications_bot_db"
-COLLECTION_NAME = "classified_records"
+DATABASE_NAME = "job-application-bot-db"
+COLLECTION_NAME = "resume-embeddings"
+
+# Embedding model
+EMBEDDING_MODEL_NAME = 'all-MiniLM-L6-v2'
+embedding_model = SentenceTransformer(EMBEDDING_MODEL_NAME)
 
 def get_database():
     client = pymongo.MongoClient(MONGODB_URI)
@@ -21,12 +31,32 @@ def get_database():
 DatabaseDependency = Annotated[pymongo.database.Database, Depends(get_database)]
 
 @app.get("/")
-def message(db: DatabaseDependency, question: Optional[str]):
-    if question:
-        if "hello" in question.lower():
-            message = "user says hello edited!"
-            db[COLLECTION_NAME].insert_one({"message": message})
-        else:
+def test(db: DatabaseDependency, question: Optional[str]):
+    try:
+        if question:
             message = "Message sent to db and received"
             db[COLLECTION_NAME].insert_one({"message": message})
-    return {"secret_message": message}
+        return {"message": message}
+    except Exception as e:
+        return {"error": f"Something went wrong {str(e)}"}
+
+
+# Pass in txt of resume and other relevant information and embed it
+@app.post("/embed-text/")
+def embed_resume_text(data: dict, db: DatabaseDependency):
+    text_input = data.get("text")
+    print(text_input)
+    try:
+        if text_input:
+            print("start embedding calculations")
+            embedding_vector = embedding_model.encode(text_input).tolist()
+            print(embedding_vector)
+            print("end embedding calculations")
+            db[COLLECTION_NAME].insert_one({
+                "embedding": embedding_vector,
+                "text": text_input,
+                "timestamp": datetime.now()
+            })
+        return {"data": data.get("text")}
+    except Exception as e:
+        return {"error": f"Something went wrong {str(e)}"}
