9759bdf1744a41b46c7874306b7c6037030e881e
yashcpanchal <yashcpanchal41@gmail.com>
2025-06-24 15:08:17 -0400

    Pivoted from initial playwright idea and now am simply using a browser extention. Will develop rapidly from here out.

--- application-backend/routers/apply.py
+++ application-backend/routers/apply.py
@@ -16,27 +16,31 @@ router = APIRouter (
 async def apply_to_job(db: DatabaseDependency, data: dict):
     url = data.get("url")
     print(f"Url is {url}")
-    async with async_playwright() as p:
-        print("Launching function")
-        browser = await p.chromium.launch()
-        
-        print("Opening browser")
-        page = await browser.new_page()
-        
-        print("Applying stealth")
-        await stealth_async(page)
-        
-        print("Navigating to url")
+    try:
+        async with async_playwright() as p:
+            print("Launching function")
+            browser = await p.chromium.launch(headless=True)
+            
+            print("Opening browser")
+            page = await browser.new_page()
+            
+            print("Applying stealth")
+            await stealth_async(page)
+            
+            print("Navigating to url")
 
-        try:
-            await page.goto(url, wait_until="domcontentloaded")
-            await page.wait_for_timeout(1000)
-            await page.screenshot(path="test.png")
-            print("Getting page title")
-            title = await page.title()
-        except Exception as e:
-            print(f"Exception caught when waiting for dom content to load: {e}")
-        finally:
-            await browser.close()
-        return {"title": title}
-    return {"message": "Something went wrong."}
+            try:
+                await page.goto(url, wait_until="domcontentloaded")
+                await page.wait_for_timeout(1000)
+                await page.screenshot(path="test.png")
+                print("Getting page title")
+                title = await page.title()
+            except Exception as e:
+                print(f"Exception caught when waiting for dom content to load: {e}")
+            finally:
+                await browser.close()
+            return {"title": title}
+        return {"message": "Something went wrong."}
+    except Exception as e:
+        print(f"Error is: {e}")
+        return {"error": str(e)}
