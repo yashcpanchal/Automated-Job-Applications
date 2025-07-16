7166f8cbc7a30e660446c189303b865f38e2ff1c
yashcpanchal <yashcpanchal41@gmail.com>
2025-07-16 18:52:41 -0400

    Update job_search.py

--- application-backend/services/job_search.py
+++ application-backend/services/job_search.py
@@ -1,9 +1,8 @@
 from typing import List
 import json
 
-
 # Langchain and Langgraph imports
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 from langchain_core.messages import BaseMessage, SystemMessage
 from typing import List
 import json
